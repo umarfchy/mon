c2445f9ebf47bf2f394bb43c50aa051aefe29431
umarfchy <umarfchy@gmail.com>
2023-09-24 00:44:01 +0600

    add entrypoint

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ A new javascript runtime inspired by node and bun ❤️
 - [ ] setup js engine (v8) (it can be using docker)
 - [ ] convert https://github.com/ErickWendel/myownnode/tree/main in golang
 
-
+- [ ] use https://github.com/rogchap/v8go to run js code
 # References
 - [Ryans first commit](https://github.com/nodejs/node/commits/main?after=55fde47b1dcd8d21ef2858f8b5d610b5b2170697+39700)
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+start_d8() {
+    d8 $*
+}
+
+start_d8_shell() {
+    OPTIONS=(--harmony
+             --harmony-shipping
+             --harmony-top-level-await
+             --harmony-intl-dateformat-day-period
+             --harmony-sharedarraybuffer
+             --harmony-atomics
+             --harmony-weak-refs
+             --harmony-string-replaceall
+             --harmony-logical-assignment
+             --harmony-atomics-waitasync
+             --harmony-relative-indexing-methods)
+
+    echo -e "\e[93mOptions\e[39m: ${OPTIONS[*]} $*"
+    echo -e "\e[93mUse quit() or Ctrl-D (i.e. EOF) to exit the D8 Shell\e[39m"
+    bash -c "sleep 0.5; rlwrap -m -pgreen d8 ${OPTIONS[*]} $*"
+}
+
+if [ "$1" = 'bash' ]; then
+    echo -e "\e[93mExecute d8 or /entrypoint.sh to start the D8 Shell\e[39m"
+    exec bash
+elif [ "$1" = 'shell' ]; then
+    shift
+    start_d8_shell $@
+elif [ "$1" = 'version' ]; then
+    echo -e "\e[92mV8 version ${V8_VERSION}\e[39m"
+elif [ "$1" = 'help' ]; then
+    echo -e "\e[92mV8 version ${V8_VERSION}\e[39m"
+    echo -e "\e[93mUsage:\e[39m"
+    echo -e "    \e[97mdocker run --rm -it andreburgaud/d8 shell\e[39m     : starts enhanced d8 shell"
+    echo -e "    \e[97mdocker run --rm -it andreburgaud/d8\e[39m           : starts default d8 shell"
+    echo -e "    \e[97mdocker run --rm -it andreburgaud/d8 bash\e[39m      : starts a bash shell"
+    echo -e "    \e[97mdocker run --rm -it andreburgaud/d8 help\e[39m      : displays this help"
+    echo -e "    \e[97mdocker run --rm -it andreburgaud/d8 version\e[39m   : displays V8 version"
+else
+    start_d8 $@
+fi
